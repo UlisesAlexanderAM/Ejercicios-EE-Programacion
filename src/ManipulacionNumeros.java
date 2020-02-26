cb4a0d8940bc14f9f357d9c30edcc52c0d478f1f
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2020-02-25 22:16:42 -0600

    Actualizacion

--- src/ManipulacionNumeros.java
+++ src/ManipulacionNumeros.java
@@ -1,8 +1,9 @@
 import java.io.Console;
 
-public class ManipulacionNumeros {
+public class ManipulacionNumeros extends Numeros {
     public static void main(String[] args) {
-        int opcion
+        int opcion,num1,num2;
+        Numeros nums = new Numeros();
         Console c =System.console();
         c.printf("Menu%n1.-Operaciones Basicas%n2.-Contar Positivos" +
                 "negativos½n3.-Contar Rangos%n4.-Contar Pares Impares%n");
@@ -14,12 +15,49 @@ public class ManipulacionNumeros {
                 do {
                     c.printf("Menu%n1.-Suma%n2.- Resta%n3.-Multiplicacion%n4.-Division");
                     opcion = Integer.parseInt(c.readLine("Ingrese el numero con la opcion elegida: "));
-                }while (opcion<1 || opcion>4)
+                    if (opcion<=0 || opcion>=5){
+                        c.printf("Opcion invalida");
+                    }else if (opcion==1){
+                        nums.setN1(Integer.parseInt(c.readLine("Ingrese el primer numero: ")));
+                        nums.setN2(Integer.parseInt(c.readLine("Ingrese el segundo numero: ")));
+                        num1=nums.getN1();
+                        num2=nums.getN2();
+                        int suma= nums.suma(num1,num2);
+                        c.printf("La suma de %d + %d es: %d",num1,num2,suma);
+                    }else if (opcion==2){
+                        nums.setN1(Integer.parseInt(c.readLine("Ingrese el primer numero: ")));
+                        nums.setN2(Integer.parseInt(c.readLine("Ingrese el segundo numero: ")));
+                        num1=nums.getN1();
+                        num2=nums.getN2();
+                        int resta= nums.resta(num1,num2);
+                        c.printf("La resta de %d - %d es: %d",num1,num2,resta);
+                    }else if (opcion==3){
+                        nums.setN1(Integer.parseInt(c.readLine("Ingrese el primer numero: ")));
+                        nums.setN2(Integer.parseInt(c.readLine("Ingrese el segundo numero: ")));
+                        num1=nums.getN1();
+                        num2=nums.getN2();
+                        int multiplicacion= nums.multiplicacion(num1,num2);
+                        c.printf("La multiplicaion de %d x %d es: %d",num1,num2,multiplicacion);
+                    }else {
+                        fnum1=Float.parseFloat(c.readLine("Ingrese el primer numero: "));
+                        do {
+                            fnum2=Float.parseFloat(c.readLine("Ingrese el segundo numero: "));
+                            if (fnum2==0){
+                                c.printf("Valor invalido, division entre 0");
+                            }
+                        }while (fnum2==0);
+                        float division= nums.division(fnum1,fnum2);
+                        c.printf("La division de %d / %d es: %d",fnum1,fnum2,division);
+                    }
+                }while (opcion<1 || opcion>4);
             }else if (opcion==2){
-
+                for (int i = 0; i <= 5; i++) {
+                    int pos,neg;
+                    num1=Integer.parseInt(c.readLine("Escribe el numero #%d",i));
+                }
             }else if (opcion==3){
 
-            }else if (opcion==4){
+            }else {
 
             }
         }while (opcion<1 || opcion>4);
--- src/Numeros.java
+++ src/Numeros.java
@@ -1,4 +1,6 @@
 class Numeros {
+    protected int n1,n2, contador;
+    protected float num1, num2;
     public int suma(int n1, int n2){
         return n1+n2;
     }
@@ -11,18 +13,11 @@ class Numeros {
     public int multiplicacion(int n1, int n2){
         return n1*n2;
     }
-    public int validarPositivo(int n){
+    public void validarPosNeg(int n, int pos, int neg){
         if (n>=0){
-            return 1;
+            pos+=1;
         } else {
-            return 1;
-        }
-    }
-    public int validarNegativo(int n){
-        if (n<0){
-            return 1;
-        } else {
-            return 1;
+            neg+=1;
         }
     }
     public int validarRango(int n, int rangoInferior, int rangoSuperior){
@@ -45,5 +40,29 @@ class Numeros {
         } else {
             return 0;
         }
+    }
+    public void setN1(int n){
+        this.n1=n;
+    }
+    public void setN2(int n2) {
+        this.n2 = n2;
+    }
+    public void setNum1(float num1) {
+        this.num1 = num1;
+    }
+    public void setNum2(float num2) {
+        this.num2 = num2;
+    }
+    public int getN1() {
+        return n1;
+    }
+    public int getN2() {
+        return n2;
+    }
+    public float getNum2() {
+        return num2;
+    }
+    public float getNum1() {
+        return num1;
     }
 }
