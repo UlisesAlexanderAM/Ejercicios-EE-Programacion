428ff3c5929dccdf87fb6390888cef96041f485f
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2020-03-02 19:02:27 -0600

    Eliminacion clase ManipulacionAlumno y adiccion del metodo en la clase Alumno

--- src/Ejercicios/Alumno.java
+++ src/Ejercicios/Alumno.java
@@ -18,4 +18,17 @@ class Alumno {
     return nombre;
   }
 
+  public static void Desempenio(float promedio) {
+    if (promedio >= 9.5 && promedio <= 10) {
+      //c.printf("%s: Excelente%n", alumno.getNombre());
+    } else if (promedio >= 8.4 && promedio <= 9.4) {
+      //c.printf("%s: Muy bien%n", alumno.getNombre());
+    } else if (promedio >= 7.5 && promedio <= 8.4) {
+      //c.printf("%s: Bien%n", alumno.getNombre());
+    } else if (promedio >= 7.0 && promedio <= 7.4) {
+      //c.printf("%s: Regular%n", alumno.getNombre());
+    } else {
+      //c.printf("%s: Mejor suerte para la proxima%n", alumno.getNombre());
+    }
+  }
 }
