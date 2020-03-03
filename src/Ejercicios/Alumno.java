b3aa78ce62f05a52c935ffa8a7469a0083a79fff
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2020-03-02 18:10:00 -0600

    Modificacion proyecto

---
+++ src/Ejercicios/Alumno.java
@@ -0,0 +1,22 @@
+package Ejercicios;
+
+class Alumno {
+  @SuppressWarnings("CanBeFinal")
+  private String nombre;
+  private float promedio;
+
+  public void calcularPromedio(int suma, int n) {
+    promedio= (float) suma / (float) n;
+  }
+  public float getPromedio(){
+    return promedio;
+  }
+  public Alumno (String nom){
+    nombre=nom;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+}
---
+++ src/Ejercicios/ManipulacionNumeros.java
@@ -0,0 +1,92 @@
+package Ejercicios;
+
+import java.io.Console;
+
+public class ManipulacionNumeros extends Numeros {
+    public static void main(String[] args) {
+        int opcion,num1,num2;
+        Numeros nums = new Numeros();
+        Console c =System.console();
+        c.printf("Menu%n1.-Operaciones Basicas%n2.-Contar Positivos" +
+                "negativos%n3.-Contar Rangos%n4.-Contar Pares Impares%n");
+        do {
+            opcion =Integer.parseInt(c.readLine("Ingrese el numero con la opcion elegida: "));
+            if (opcion<=0 || opcion>=5){
+                c.printf("Opcion invalida");
+            }else if (opcion==1){
+                do {
+                    c.printf("Menu%n1.-Suma%n2.- Resta%n3.-Multiplicacion%n4.-Division%n");
+                    opcion = Integer.parseInt(c.readLine("Ingrese el numero con la opcion elegida: "));
+                    if (opcion<=0 || opcion>=5){
+                        c.printf("Opcion invalida");
+                    }else if (opcion==1){
+                        nums.setN1(Integer.parseInt(c.readLine("Ingrese el primer numero: ")));
+                        nums.setN2(Integer.parseInt(c.readLine("Ingrese el segundo numero: ")));
+                        num1=nums.getN1();
+                        num2=nums.getN2();
+                        int suma= nums.suma(num1,num2);
+                        c.printf("La suma de %d + %d es: %d%n",num1,num2,suma);
+                    }else if (opcion==2){
+                        nums.setN1(Integer.parseInt(c.readLine("Ingrese el primer numero: ")));
+                        nums.setN2(Integer.parseInt(c.readLine("Ingrese el segundo numero: ")));
+                        num1=nums.getN1();
+                        num2=nums.getN2();
+                        int resta= nums.resta(num1,num2);
+                        c.printf("La resta de %d - %d es: %d%n",num1,num2,resta);
+                    }else if (opcion==3){
+                        nums.setN1(Integer.parseInt(c.readLine("Ingrese el primer numero: ")));
+                        nums.setN2(Integer.parseInt(c.readLine("Ingrese el segundo numero: ")));
+                        num1=nums.getN1();
+                        num2=nums.getN2();
+                        int multiplicacion= nums.multiplicacion(num1,num2);
+                        c.printf("La multiplicacion de %d x %d es: %d%n",num1,num2,multiplicacion);
+                    }else {
+                        nums.setNum1(Float.parseFloat(c.readLine("Ingrese el primer numero: ")));
+                        float fnum1=nums.getNum1();
+                        float fnum2;
+                        do {
+                            nums.setNum2(Float.parseFloat(c.readLine("Ingrese el segundo numero: ")));
+                            fnum2=nums.getNum2();
+                            if (fnum2==0){
+                                c.printf("Valor invalido, division entre 0");
+                            }
+                        }while (fnum2==0);
+                        float division= nums.division(fnum1,fnum2);
+                        c.printf("La division de %f / %f es: %f%n",fnum1,fnum2,division);
+                    }
+                }while (opcion<1 || opcion>4);
+            }else if (opcion==2){
+                int pos=0,neg=0;
+                for (int i = 0; i < 5; i++) {
+                    num1=Integer.parseInt(c.readLine("Escribe el numero #%d: ",i+1));
+                    nums.validarPosNeg(num1,pos,neg);
+                    pos=nums.getPos();
+                    neg=nums.getNeg();
+                }
+                c.printf("Positivos: %d, Negativos: %d%n",pos,neg);
+            }else if (opcion==3){
+                int rango10=0,rango100=0,rangoMayor100=0;
+                for (int i = 0; i < 10; i++) {
+                    do {
+                        num1=Integer.parseInt(c.readLine("Escribe el numero #%d: ",i+1));
+                    }while (num1<=0);
+                    rango10+=nums.validarRango(num1,1,10);
+                    rango100+=nums.validarRango(num1,11,100);
+                    rangoMayor100+=nums.validarRango(num1,101);
+                }
+                c.printf("Numeros entre 1-10: %d%nNumeros entre 11-100: %d%nNumeros mayores a 100:%d%n",rango10,rango100,rangoMayor100);
+            }else {
+                int pares=0,impares=0;
+                for (int i = 0; i < 10; i++) {
+                    do {
+                        num1=Integer.parseInt(c.readLine("Escribe el numero #%d: ",i+1));
+                    }while (num1<=0);
+                    nums.validarParesImpares(num1,pares,impares);
+                    pares=nums.getP();
+                    impares=nums.getI();
+                }
+                c.printf("Pares: %d, Impares: %d%n",pares,impares);
+            }
+        }while (opcion<1 || opcion>4);
+    }
+}
