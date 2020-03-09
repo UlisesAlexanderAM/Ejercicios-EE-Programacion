c4b2b737f8d54dfaea3a037ba23f0ae5a6a43f71
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2020-03-09 07:12:31 -0600

    Sustitución de un arreglo de una dimension por uno de 3 dimensiones, para el arreglo de CCuenta

--- src/Arreglo3Dimensiones/PrincipalA3D.java
+++ src/Arreglo3Dimensiones/PrincipalA3D.java
@@ -10,24 +10,20 @@ public class PrincipalA3D {
     public static void main(String[] args) {
         double dinero;
         String[][] matrix = getReferencias();
-        CCuenta[] ventasTotales = new CCuenta[8];
-        int i=0;
-        for (int j = 0; j < 2; j++) {
-            for (int k = 0; k < 2; k++) {
-                for (int l = 0; l < 2; l++) {
-                    dinero = Leer.leerDouble("Ingrese las ventas totales de %s del %s de la %s: ", matrix[0][l], matrix[1][k], matrix[2][j]);
-                    ventasTotales[i] = new CCuenta(dinero);
-                    i++;
+        CCuenta[][][] ventasTotales = new CCuenta[2][2][2];
+        for (int k = 0; k < 2; k++) {
+            for (int j = 0; j < 2; j++) {
+                for (int i = 0; i < 2; i++) {
+                    dinero = Leer.leerDouble("Ingrese las ventas totales de %s del %s de la %s: ", matrix[0][i], matrix[1][j], matrix[2][k]);
+                    ventasTotales[i][j][k] = new CCuenta(dinero);
                 }
             }
         }
-        i=0;
         Imprimir.Imprimir("%n");
-        for (int j = 0; j < 2; j++) {
-            for (int k = 0; k < 2; k++) {
-                for (int l = 0; l < 2; l++) {
-                    Imprimir.Imprimir("Las ventas totales de %s del %s de la %s son: %f %n",matrix[0][l], matrix[1][k], matrix[2][j], ventasTotales[i].getSaldo());
-                    i++;
+        for (int k = 0; k < 2; k++) {
+            for (int j = 0; j < 2; j++) {
+                for (int i = 0; i < 2; i++) {
+                    Imprimir.Imprimir("Las ventas totales de %s del %s de la %s son: %f %n",matrix[0][i], matrix[1][j], matrix[2][k], ventasTotales[i][j][k].getSaldo());
                 }
             }
         }
